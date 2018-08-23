4017c535ed4c77a34000cb183c5a27c8b850d2ef
johnwmccallum <johnwmccallum@gmail.com>
2018-08-23 12:50:46 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -138,6 +138,15 @@ $(function () {
               }]
           },
           options: {
+            tooltips: {
+                callbacks: {
+                    label: function(tooltipItem, data) {
+                        return "$" + Number(tooltipItem.yLabel).toFixed(0).replace(/./g, function(c, i, a) {
+                            return i > 0 && c !== "." && (a.length - i) % 3 === 0 ? "," + c : c;
+                        });
+                    }
+                }
+              },
               legend: {
                   display: false
               },
